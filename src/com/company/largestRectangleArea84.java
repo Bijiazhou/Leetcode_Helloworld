efd39794d52ba49bcbbfc85543dc3d39fb4016e8
岳晨阳 <805695510@qq.com>
2021-03-13 22:19:01 +0800

    change：add new questions and solutions 栈和队列

--- src/com/company/largestRectangleArea84.java
+++ src/com/company/largestRectangleArea84.java
@@ -5,6 +5,7 @@ package com.company;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class largestRectangleArea84 {
     //思路1 刚开始想的双指针 ，但是又和蓄水那道题不太一样 暴力法呢 for for ？？超时！！！
@@ -80,17 +81,48 @@ public class largestRectangleArea84 {
     }
 
 
-    //奇怪思路 双端队列？
+    //奇怪思路 官方解法
     public int largestRectangleArea4(int[] heights) {
-        int maxArea = -1;
-        Deque<Integer> deque = new LinkedList<Integer>();
-        for (int i = 0; i < heights.length; i++) {
-            deque.addFirst(heights[i]);
-        }
+            // 初始化最终结果为0
+            int res = 0;
+            Stack<Integer> stack = new Stack<>();
 
-//        whi
+            // 将给定的原数组左右各添加一个元素0
+            int[] newHeights = new int[heights.length + 2];
+            newHeights[0] = 0;
+            newHeights[newHeights.length-1] = 0;
+            for (int i = 1; i < heights.length + 1; i++) {
+                newHeights[i] = heights[i - 1];
+            }
 
-        return 0;
+            // 开始遍历
+            for (int i = 0; i < newHeights.length; i++) {
+                // 如果栈不为空且当前考察的元素值小于栈顶元素值，
+                // 则表示以栈顶元素值为高的矩形面积可以确定
+                while (!stack.isEmpty() && newHeights[i] < newHeights[stack.peek()]) {
+                    // 弹出栈顶元素
+                    int cur = stack.pop();
+                    // 获取栈顶元素对应的高
+                    int curHeight = newHeights[cur];
+
+                    // 栈顶元素弹出后，新的栈顶元素就是其左侧边界
+                    int leftIndex = stack.peek();
+                    // 右侧边界是当前考察的索引
+                    int rightIndex = i;
+                    // 计算矩形宽度
+                    int curWidth = rightIndex - leftIndex - 1;
+
+                    // 计算面积
+                    res = Math.max(res, curWidth * curHeight);
+                }
+
+                // 当前考察索引入栈
+                stack.push(i);
+            }
+
+            return res;
+
+
     }
     public static void main(String[] args) {
         int[] nums = new int[]{2,1,5,6,2,3};
